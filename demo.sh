f7a88f7f42ea2cfb868e92fba3327b9b082b076d
Brian Dodd <104315834+USH3R@users.noreply.github.com>
2026-03-17 15:24:02 -0400

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -2,10 +2,12 @@
 
 echo "=== SHOCKACON Demo Start ==="
 
+# Compile the C++ Enclave Guard
+g++ enclaveguard.cpp -o enclave_guard
+
 # Step 1: Python
 echo "[PYTHON] Running Sentinel Engine..."
-# optional: install dependencies (only first time)
-# pip install -r requirements.txt
+pip install -r requirements.txt
 python3 ShakaCon.py
 
 # Step 2: Java
@@ -15,7 +17,6 @@ java DigitalMentor
 
 # Step 3: C++
 echo "[C++] Running Enclave Guard..."
-g++ -o enclave_guard enclave_guard.cpp
 ./enclave_guard
 
 echo "=== SHOCKACON Demo Complete ==="
